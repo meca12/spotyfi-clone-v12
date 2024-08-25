93d550f637cae7c0f903e653a640761a92baedfa
Martha  avila <itit.martha.avil@gmail.com>
2024-08-25 15:05:42 -0600

    archivos base

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderUserComponent,
+    MediaPlayerComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
