c93b6f0370d8d39180fb6b1818d6aee7c1183a04
Martha  avila <itit.martha.avil@gmail.com>
2024-08-25 21:27:53 -0600

    archivos de rutas y modulos

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './page/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [{path:'', component:HomePageComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HomePageComponent } from './page/home-page/home-page.component';
 import { HomeRoutingModule } from './home-routing.module';
-
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
